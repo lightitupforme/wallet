ef2f210fecce8e9e659ffee95ba9c26a50197379
Ole Bläsing <ole.blaesing@web.de>
2016-07-21 18:06:41 +0200

    Remove reload button

--- src/components/App.js
+++ src/components/App.js
@@ -16,10 +16,6 @@ class App extends React.Component {
     this.logout = this.logout.bind(this);
   }
 
-  refresh() {
-    location.reload();
-  }
-
   logout() {
     this.props.logout();
     this.props.router.push('/');
@@ -28,7 +24,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <button type="button" onClick={this.refresh}>refresh</button>
         <BalanceContainer />
         <button type="button" onClick={this.logout}>logout</button>
         <TabBar tabs={tabs} />
