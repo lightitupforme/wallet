aa50635a3f41502709a9a3050638de47c89287a7
Ole Bläsing <ole.blaesing@web.de>
2016-07-21 14:46:10 +0200

    Display datetime

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+export const timestampToDateTime = timestamp => {
+  const locale = new Intl.DateTimeFormat('de-DE', {
+    weekday: 'short',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  });
+
+  return locale.format(new Date(timestamp * 1000));
+};
+
 const renderTransactionList = transactions => transactions.map((transaction, i) => (
   <tr key={i}>
+    <td>{timestampToDateTime(transaction.time)}</td>
     <td>{transaction.category}</td>
     <td>{transaction.amount}</td>
     <td>{transaction.address}</td>
@@ -13,7 +27,8 @@ const TransactionList = ({ transactions }) => (
   <table>
     <thead>
       <tr>
-        <th></th>
+        <th>date</th>
+        <th>type</th>
         <th>amount</th>
         <th>address</th>
         <th>confirmations</th>
